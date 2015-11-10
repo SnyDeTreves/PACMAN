5a89fe5f1c2699896df6a623477307b0e5886f2d
laiaga <leonardialexandre@free.fr>
2015-11-10 02:18:36 +0100

    correcs

--- gameplay/main.cpp
+++ gameplay/main.cpp
@@ -8,16 +8,31 @@ using namespace gameplay;
 
 int main(int argc, char *argv[])
 {
+   // std::cout << "un" << std::endl;
 
     Core_kernel kernel(argc,argv);
 
+   // std::cout << "deux" << std::endl;
+
     Main_window w;
+
+   // std::cout << "trois" << std::endl;
+
     w.display_background("ressources/pacman_layout.JPG");
+
+   // std::cout << "quatre" << std::endl;
+
     w.show();
 
+   // std::cout << "cinq" << std::endl;
 
     Pacman pacman(0,0,"ressources/pacman.png");
+
+   // std::cout << "six" << std::endl;
+
     w.get_play_zone()->display_character(pacman);
+
+   // std::cout << "sept" << std::endl;
 
     return kernel.exec();
 }
