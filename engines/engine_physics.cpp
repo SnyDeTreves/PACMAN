3c9f9d88631997f8ec7e46ec507e3a41adec9368
Sny_de_Treves <sny.de.treves@gmail.com>
2015-12-02 20:24:34 +0100

    collision

--- engines/engine_physics.cpp
+++ engines/engine_physics.cpp
@@ -10,11 +10,6 @@ bool Engine_physics::is_collision(Entity e1, Entity e2)
     }
     else
     {
-        int dif_pos_w = abs(e1.pos().x() - e2.pos().x());
-        int dif_pos_h = abs(e1.pos().y() - e2.pos().y());
-        int dif_box_w = abs(e1.get_box().get_width() + e2.get_box().get_width());
-        int dif_box_h = abs(e1.get_box().get_height() + e2.get_box().get_height());
-
-        return (dif_pos_w <= dif_box_w/2 && dif_pos_h <= dif_box_h/2);
+        return e1.collidesWithPath(e2.clipPath());
     }
 }
