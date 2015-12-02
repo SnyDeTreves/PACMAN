4ae09c1c527d866cca2a98394b4acbb27f7284c0
JimArchiviste <JimArchiviste@users.noreply.github.com>
2015-12-02 21:38:56 +0100

    Create ia.h

---
+++ engines/ia.h
@@ -0,0 +1,16 @@
+#ifndef IA_H
+#define IA_H
+
+#include <QPoint>
+#include <QString>
+
+class ia
+{
+    public:
+        ia();
+        QPoint suivrePacman();
+        QPoint bloquerPacman();
+
+};
+
+#endif // IA_H
