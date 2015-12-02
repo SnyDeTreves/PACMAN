2b71fa02d1188c8fa2631f40dec6c95297940c51
JimArchiviste <JimArchiviste@users.noreply.github.com>
2015-12-02 14:00:44 +0100

    Create ennemi.h

---
+++ engines/ennemi.h
@@ -0,0 +1,32 @@
+#ifndef ENNEMI
+#define ENNEMI
+
+#include <QPoint>
+#include <QString>
+#include "iheading.h"
+#include "heading_east.h"
+#include "heading_north.h"
+#include "heading_south.h"
+#include "heading_west.h"
+#include "unmoving.h"
+#include "entity.h"
+#include "box.h"
+
+
+namespace engine
+{
+    class Ennemi : public Entity
+    {
+    private:
+    protected:
+        IHeading *heading;
+    public:
+        Ennemi(int x, int y, int width, int height, QString img_path);
+        Ennemi(QPoint pos, Box box, QString img_path);
+        void moveOn();
+    };
+}
+
+
+#endif // ENNEMI
+
