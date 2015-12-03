77bab1d4c85734cb85c2b19aec10eae5fa9634f7
UltimateCookie <loic.vierin@gmail.com>
2015-12-03 22:48:26 +0100

    Premier test d ajout des PacDots

--- gameplay/main.cpp
+++ gameplay/main.cpp
@@ -10,6 +10,8 @@
 using namespace engine;
 using namespace gameplay;
 
+# define GAP 13
+
 int main(int argc, char *argv[])
 {
     Core_kernel ker(argc,argv);
@@ -22,15 +24,35 @@ int main(int argc, char *argv[])
 
     graph.add_entity(p);
 
+//PLACEMENT DES POINTS
+    int x=58;
+    int y=57;
+    QPoint pos_dot(x,y);
+    std::vector<PacDot*> pacDot_list = std::vector<PacDot*>();
 
 
-    QPoint pos_d1(310,310);
-    PacDot d1(pos_d1,":/ressources/dot.png");
-    graph.add_entity(d1);
+    for(int i=0; i<12; i++) {
+        x += GAP;
+        pos_dot.setX(x);
+        pos_dot.setY(y);
+        PacDot* dot = new PacDot(pos_dot,":/ressources/dot.png");
+        pacDot_list.push_back(dot);
+
+
+    }
+
+
+
+    for(PacDot* dot : pacDot_list)
+    {
+        graph.add_entity(*dot);
+        ker.add_entity(*dot);
+    }
+
+
 
 
     ker.add_entity(p);
-    ker.add_entity(d1);
 
 
 
